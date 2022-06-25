062b762598a304c0d6bbd5bb69571c0da99889f9
daregoodness1997 <daregoodness@gmail.com>
2022-06-25 19:16:26 +0100

    eui button component added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import '@elastic/eui/dist/eui_theme_light.css';
+
+import { EuiProvider } from '@elastic/eui';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <EuiProvider colorMode='dark'>
     <App />
-  </React.StrictMode>
-)
+  </EuiProvider>
+);
---
+++ src/ui-components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { EuiButton } from '@elastic/eui';
+
+const Button = () => {
+  return (
+    <EuiButton color='primary' size='s'>
+      Label
+    </EuiButton>
+  );
+};
+
+export default Button;
