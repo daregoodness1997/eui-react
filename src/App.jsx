ff597fd3008a13c5badede86c016766af06795ed
daregoodness1997 <daregoodness@gmail.com>
2022-06-25 19:42:11 +0100

    app page errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from './ui-components/Button';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className='App'>
       <Button />
     </div>
   );
 }
+
+export default App;
